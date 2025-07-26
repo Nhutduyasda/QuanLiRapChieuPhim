9e4458eb3d1b1bcf3c376777ab6b998a8047caff
Nhutduyasda <151909043+Nhutduyasda@users.noreply.github.com>
2025-07-26 07:49:30 +0700

    Add VeDTO

---
+++ DTO_Model/VeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_Model
+{
+     public class VeDTO
+    {
+        public string? MaVe { get; set; } // Mã vé
+        public string? MaDatVe { get; set; } // Mã đặt vé
+        public string? MaSuatChieu { get; set; } // Mã suất chiếu
+        public string? MaGhe { get; set; } // Mã ghế
+        public decimal GiaVe { get; set; } // Giá vé
+        public string? LoaiVe { get; set; } // Loại vé: 'Người lớn', 'Trẻ em', 'Người cao tuổi'
+    }
+}
